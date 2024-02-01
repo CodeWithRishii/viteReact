2a8caef28264b8a88b63bc33d71381545bb0062a
Rishii <rishibhavi8368@gmail.com>
2024-02-01 15:00:38 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import "./App.css";
+// import HtmlIcon from "@mui/icons-material/Html";
+
+function App() {
+  return (
+    <>
+      <nav className="navbar">
+        <div className="logo">
+         <img src="https://cdn-icons-png.flaticon.com/128/3800/3800024.png" alt="" />
+        </div>
+        <ul>
+          <li>Home</li>
+          <li>About</li>
+          <li>Contact</li>
+          <li>SignUp </li>
+        </ul>
+      </nav>
+    </>
+  );
+}
+
+export default App;
